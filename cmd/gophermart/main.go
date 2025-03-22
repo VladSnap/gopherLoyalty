3a4edf3a4e2c9a356303a3fa75e6889f74db88df
Vladislav Negmatulin <negmatulin@mego.travel>
2025-03-22 15:37:53 +0300

    add main and app

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+import (
+	"github.com/VladSnap/gopherLoyalty/internal/application"
+)
+
+func main() {
+	application.Run()
+}
---
+++ internal/application/app.go
@@ -0,0 +1,46 @@
+package application
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/VladSnap/gopherLoyalty/internal/application/config"
+	"github.com/VladSnap/gopherLoyalty/internal/infrastructure/log"
+	"github.com/VladSnap/gopherLoyalty/internal/infrastructure/services"
+	"go.uber.org/zap"
+)
+
+var resourceManager *services.ResourceManager
+
+func Run() {
+	resourceManager = services.NewResourceManager()
+	defer func() {
+		err := resourceManager.Cleanup()
+
+		if err != nil {
+			panic(fmt.Errorf("failed resourceManager clean: %w", err))
+		}
+	}()
+	// Регаем функцию Sync Zap логов
+	resourceManager.Register(log.Close)
+
+	log.Zap.Info("run shorneter server", zap.Strings("Args", os.Args))
+
+	confValidator := &config.OptionsValidator{}
+	conf, err := config.LoadConfig(confValidator)
+	if err != nil {
+		panic(err)
+	}
+
+	apiServer, err := CreateApiServer(conf, resourceManager)
+	if err != nil {
+		panic(err)
+	}
+
+	err = apiServer.Start()
+
+	if err != nil {
+		log.Zap.Errorf("failed stop server: %w", err)
+	}
+	log.Zap.Info("app.Run end")
+}
