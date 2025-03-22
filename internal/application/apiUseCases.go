decb05538457ddfb7a2b906ab2aa2136d721f079
Vladislav Negmatulin <negmatulin@mego.travel>
2025-03-22 15:37:00 +0300

    add interface for use cases

---
+++ internal/application/apiUseCases.go
@@ -0,0 +1,41 @@
+package application
+
+import (
+	"context"
+
+	"github.com/VladSnap/gopherLoyalty/internal/features/getBalance"
+	"github.com/VladSnap/gopherLoyalty/internal/features/getOrders"
+	"github.com/VladSnap/gopherLoyalty/internal/features/getWithdrawals"
+	"github.com/VladSnap/gopherLoyalty/internal/features/loginUser"
+	"github.com/VladSnap/gopherLoyalty/internal/features/registrationUser"
+	"github.com/VladSnap/gopherLoyalty/internal/features/withdrawBalance"
+	"github.com/VladSnap/gopherLoyalty/internal/infrastructure/api"
+)
+
+type RegistrationUserUseCase interface {
+	Execute(ctx context.Context, input *registrationUser.RegisterRequest, output *api.EmptyBody) error
+}
+
+type LoginUserUseCase interface {
+	Execute(ctx context.Context, input *loginUser.LoginUserRequest, output *api.EmptyBody) error
+}
+
+type UploadOrderUseCase interface {
+	Execute(ctx context.Context, input string, output *api.EmptyBody) error
+}
+
+type GetOrdersUseCase interface {
+	Execute(ctx context.Context, input *interface{}, output *getOrders.OrderListResponse) error
+}
+
+type GetBalanceUseCase interface {
+	Execute(ctx context.Context, input *interface{}, output *getBalance.BalanceResponse) error
+}
+
+type WithdrawBalanceUseCase interface {
+	Execute(ctx context.Context, input *withdrawBalance.WithdrawRequest, output *api.EmptyBody) error
+}
+
+type GetWithdrawalsUseCase interface {
+	Execute(ctx context.Context, input *interface{}, output *getWithdrawals.WithdrawalListResponse) error
+}
